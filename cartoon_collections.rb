c69b4e73bfd5091056aadc4970018b5479c7447e
Chris Ashman <chrisashman96@gmail.com>
2020-01-28 00:36:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,6 +25,7 @@ def find_the_cheese(potentially_cheesy_items)
   
   potentially_cheesy_items.find do |maybe_cheese|
     cheese_types.include?(maybe_cheese)
+  end
 end
 
 find_the_cheese(potentially_cheesy_items)
